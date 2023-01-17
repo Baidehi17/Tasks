1e70c0b671b7c4fdd9aaad5e568325f1b128d1ed
Baidehi17 <baidehi.s@Technovert.com>
2023-01-17 15:57:35 +0530

    EMP

--- src/app/employee-director/emp-result/emp-result.component.ts
+++ src/app/employee-director/emp-result/emp-result.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { EmpService } from './../../emService/emp.service';
+import { Empclass } from './../../emService/empclass';
 @Component({
-  selector: 'app-emp-result',
-  templateUrl: './emp-result.component.html',
-  styleUrls: ['./emp-result.component.scss']
+     selector: 'app-emp-result',
+     templateUrl: './emp-result.component.html',
+     styleUrls: ['./emp-result.component.scss']
 })
-export class EmpResultComponent {
+export class EmpResultComponent implements OnInit {
 
+     constructor(private empservice: EmpService) { }
+     employeeList!: Empclass[];
+     ngOnInit() {
+
+          
+          this.empservice.getcommants()
+               .subscribe
+               (
+                    data => {
+                         this.employeeList = data;
+                    }
+               );
+     }
+
+     
 }
